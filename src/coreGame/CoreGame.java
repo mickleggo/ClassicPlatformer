98a3c0bb1cf1f83db065ba5e5c27f93ec7204b91
mickleggo <mickleggo@hotmail.com>
2023-11-16 20:31:27 +1030

    implement game loop (video 02)

--- src/coreGame/CoreGame.java
+++ src/coreGame/CoreGame.java
@@ -1,6 +1,8 @@
 package coreGame;
-
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 public class CoreGame extends Canvas implements Runnable {
 	private static final long serialVersionUID = -6764672190690957850L;
@@ -21,9 +23,56 @@ public class CoreGame extends Canvas implements Runnable {
 	}
 	
 	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int updates = 0;
+		int frames = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1) {
+				tick();
+				updates++;
+				delta--;
+			}
+			render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+//				fps = frames;
+//				ticks = updates;
+				System.out.println("FPS: " + frames + "  | TICKS: " + updates);
+				frames = 0;
+				updates = 0;
+			}
+		}
+	}
+	
+	private void tick() {
 		
 	}
 	
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+	//***************************************************************************************//	
+		//Draw game here
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+	//***************************************************************************************//	
+		g.dispose();
+		bs.show();
+	}
 	
-
 }
