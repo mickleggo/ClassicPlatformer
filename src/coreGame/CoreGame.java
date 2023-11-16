46062e9ccd0df28a40e3455c8091d11214b802f4
mickleggo <mickleggo@hotmail.com>
2023-11-16 20:01:48 +1030

    completed initial game window (video 01)

--- src/coreGame/CoreGame.java
+++ src/coreGame/CoreGame.java
@@ -1,9 +1,29 @@
 package coreGame;
 
-public class CoreGame {
+import java.awt.Canvas;
+
+public class CoreGame extends Canvas implements Runnable {
+	private static final long serialVersionUID = -6764672190690957850L;
 
 	public static void main(String[] args) {
-
+		new Window(800, 600, "Platformer", new CoreGame());
 	}
+	
+	private boolean running = false;
+	private Thread thread;
+	
+	public synchronized void start() {
+		if(running) return;
+		
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public void run() {
+		
+	}
+	
+	
 
 }
