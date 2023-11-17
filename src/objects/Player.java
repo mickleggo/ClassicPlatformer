70f66aaca236c5247f4a7d7184fc8e52c84bb7e5
mickleggo <mickleggo@hotmail.com>
2023-11-17 17:48:10 +1030

    Add player bounding box. (video 07)

--- src/objects/Player.java
+++ src/objects/Player.java
@@ -1,8 +1,10 @@
 package objects;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.util.LinkedList;
+
 import framework.GameObject;
 import framework.ObjectId;
 
@@ -22,7 +24,7 @@ public class Player extends GameObject {
 
 	public void tick(LinkedList<GameObject> object) {
 		x += velX;
-		y += velY;
+		//y += velY;
 		
 		if(jumping || falling) {
 			velY += gravity;
@@ -33,10 +35,26 @@ public class Player extends GameObject {
 	public void render(Graphics g) {
 		g.setColor(Color.BLUE);
 		g.fillRect((int) x, (int) y, (int) width, (int) height);
+		
+		Graphics2D g2d = (Graphics2D) g;
+		g.setColor(Color.MAGENTA);
+		g2d.draw(getBounds());
+		g2d.draw(getBoundsLeft());
+		g2d.draw(getBoundsRight());
+		g2d.draw(getBoundsTop());
 	}
 
 	public Rectangle getBounds() {
-		return new Rectangle((int) x, (int) y, (int) width, (int) height);
+		return new Rectangle((int) x+2, (int) y+4, (int) width-5, (int) height-4);
+	}
+	public Rectangle getBoundsLeft() {
+		return new Rectangle((int) x-1, (int) y+4, (int) 4, (int) height-8);
+	}
+	public Rectangle getBoundsRight() {
+		return new Rectangle((int) x+ (int)width -4, (int) y+4, (int) 4, (int) height-8);
+	}
+	public Rectangle getBoundsTop() {
+		return new Rectangle((int) x, (int) y-1, (int) width-1, (int) 4);
 	}
 
 }
